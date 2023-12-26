f948d76f635af187285b3bccc0f4212338fc46a5
Sai Krishna <99529667+saikrishna7783@users.noreply.github.com>
2023-12-26 15:05:09 +0530

    Update Recursive Parser Descent.c

--- Recursive Parser Descent.c
+++ Recursive Parser Descent.c
@@ -1,112 +1,85 @@
-    #include <stdio.h>  
-    #include <string.h>  
-      
-    #define SUCCESS 1  
-    #define FAILED 0  
-      
-    int E(), Edash(), T(), Tdash(), F();  
-      
-    const char *cursor;  
-    char string[64];  
-      
-    int main()  
-    {  
-        puts("Enter the string");  
-        // scanf("%s", string);  
-        sscanf("i+(i+i)*i", "%s", string);  
-        cursor = string;  
-        puts("");  
-        puts("Input      Action");  
-        puts("--------------------------------");  
-      
-        if (E() && *cursor == '\0') {  
-            puts("--------------------------------");  
-            puts("String is successfully parsed");  
-            return 0;  
-        } else {  
-            puts("--------------------------------");  
-            puts("Error in parsing String");  
-            return 1;  
-        }  
-    }  
-      
-    int E()  
-    {  
-        printf("%-16s E -> T E'\n", cursor);  
-        if (T()) {  
-            if (Edash())  
-                return SUCCESS;  
-            else  
-                return FAILED;  
-        } else  
-            return FAILED;  
-    }  
-      
-    int Edash()  
-    {  
-        if (*cursor == '+') {  
-            printf("%-16s E' -> + T E'\n", cursor);  
-            cursor++;  
-            if (T()) {  
-                if (Edash())  
-                    return SUCCESS;  
-                else  
-                    return FAILED;  
-            } else  
-                return FAILED;  
-        } else {  
-            printf("%-16s E' -> $\n", cursor);  
-            return SUCCESS;  
-        }  
-    }  
-      
-    int T()  
-    {  
-        printf("%-16s T -> F T'\n", cursor);  
-        if (F()) {  
-            if (Tdash())  
-                return SUCCESS;  
-            else  
-                return FAILED;  
-        } else  
-            return FAILED;  
-    }  
-      
-    int Tdash()  
-    {  
-        if (*cursor == '*') {  
-            printf("%-16s T' -> * F T'\n", cursor);  
-            cursor++;  
-            if (F()) {  
-                if (Tdash())  
-                    return SUCCESS;  
-                else  
-                    return FAILED;  
-            } else  
-                return FAILED;  
-        } else {  
-            printf("%-16s T' -> $\n", cursor);  
-            return SUCCESS;  
-        }  
-    }  
-      
-    int F()  
-    {  
-        if (*cursor == '(') {  
-            printf("%-16s F -> ( E )\n", cursor);  
-            cursor++;  
-            if (E()) {  
-                if (*cursor == ')') {  
-                    cursor++;  
-                    return SUCCESS;  
-                } else  
-                    return FAILED;  
-            } else  
-                return FAILED;  
-        } else if (*cursor == 'i') {  
-            cursor++;  
-            printf("%-16s F ->i\n", cursor);  
-            return SUCCESS;  
-        } else  
-            return FAILED;  
-    }  
+#include <stdio.h>
+#include <string.h>
+
+#define SUCCESS 1
+#define FAILED 0
+
+char *cursor;
+char string[64];
+
+int A()
+{
+    if (*cursor == 'a')
+    {
+        cursor++;
+        if ((*cursor) == 'b')
+        {
+            cursor++;
+            printf("%-16s A -> ab\n", cursor);
+        }
+        else
+        {
+            printf("%-16s A -> a\n", cursor);
+        }
+
+        return SUCCESS;
+    }
+    else
+    {
+        return FAILED;
+    }
+}
+
+int S()
+{
+    printf("%-16s S -> cAd\n", cursor);
+
+    if (*cursor == 'c')
+    {
+        cursor++;
+        if (A())
+        {
+            if (*cursor == 'd')
+            {
+                printf("%-16s S -> cAd\n", "EOF");
+                cursor++;
+                return SUCCESS;
+            }
+            else
+            {
+                return FAILED;
+            }
+        }
+        else
+        {
+            return FAILED;
+        }
+    }
+    else
+    {
+        return FAILED;
+    }
+}
+
+int main()
+{
+    printf("Enter the string: ");
+    scanf("%s", string);
+    cursor = string;
+    puts("");
+    puts("Input          Action");
+    puts("--------------------------------");
+
+    if (S() && *cursor == '\0')
+    {
+        puts("--------------------------------");
+        puts("String is successfully parsed");
+        return 0;
+    }
+    else
+    {
+        puts("--------------------------------");
+        puts("Error in parsing String");
+        return 1;
+    }
+}
